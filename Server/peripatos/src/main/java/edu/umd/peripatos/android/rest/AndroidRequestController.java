9c969a8623e1c60ebfcdcb8f97b3d150aa2ebd7e
helios2k6 <helios2k6@gmail.com>
2011-11-29 16:15:21 -0500

    modified pom and stuff

--- Server/peripatos/src/main/java/edu/umd/peripatos/android/rest/AndroidRequestController.java
+++ Server/peripatos/src/main/java/edu/umd/peripatos/android/rest/AndroidRequestController.java
@@ -1,11 +1,19 @@
 package edu.umd.peripatos.android.rest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/android/request/")
+@RequestMapping("/android/")
 public class AndroidRequestController {
 	
-	
+	@RequestMapping(value="/request", method=RequestMethod.POST)
+	@ResponseBody
+	public String getUserInformation(Model model){
+		
+		return "";
+	}
 }
